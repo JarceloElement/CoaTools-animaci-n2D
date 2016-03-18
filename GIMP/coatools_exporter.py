f417401b9240f6215dedfd1aeaf4a7f258b1d998
Ragnar Brynjulfsson <me@ragnarb.com>
2016-03-18 01:40:51 +0100

    WIP Refactoring and positioning tiles.

--- GIMP/coatools_exporter.py
+++ GIMP/coatools_exporter.py
@@ -40,6 +40,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+import os
 from math import floor, sqrt, ceil
 from gimpfu import *
 
@@ -56,92 +57,109 @@ class Sprite():
         print('Tilesize: {tilesize}'.format(tilesize=self.tilesize))
 
 
-def export_to_coatools(img, drw):
-    # TODO! Make this whole operation one undo
-    # Loop through visible layers
-    sprites = {}
-    for layer in img.layers:
-        if layer.visible:
-            if len(layer.children) > 0:
-                minx, miny, maxx, maxy = 100000, 100000, 0, 0
-                frames = 0
-                for child in layer.children:
-                    if len(child.children) > 0:
-                        print("Nested layer groups not supported")
-                        continue
-                    pdb.plug_in_autocrop_layer(img, child)
-                    frames = frames + 1
-                    x1, y1 = child.offsets
-                    x2 = child.width + x1
-                    y2 = child.height + y1
-                    if x1 < minx:
-                        minx = x1
-                    if y1 < miny:
-                        miny = y1
-                    if x2 > maxx:
-                        maxx = x2
-                    if y2 > maxy:
-                        maxy = y2
-                width = maxx - minx
-                height = maxy - miny
-                # Calculate optimal tiles and image size.
-                gridx = floor(sqrt(frames))
-                gridy = ceil(frames / gridx)
-                img2 = gimp.Image(int(width * gridx), int(height * gridy))
-                col = 1
-                row = 1
-                print('GRIDX {gridx}'.format(gridx=gridx))
-                for child in layer.children:
-                    print('COL {col}, ROW {row}'.format(col=col, row=row))
-                    if len(child.children) > 0:
-                        continue
-                    if col % gridx > 0:
-                        # TODO! Same as below, move copy/paste function out.
-                        col = col + 1
-                    else:
-                        # Copy layer to col, row with correct offset
+class CoaExport():
+    def __init__(self, img):
+        self.img = img
+        self.sprites = {}
+
+
+    def paste_layer(self, img, name, x, y):
+        ''' Paste a layer with offset '''
+        layer = img.new_layer(name,
+                                 img.width,
+                                 img.height)
+        floating_layer = pdb.gimp_edit_paste(layer, True)
+        pdb.gimp_floating_sel_anchor(floating_layer)
+        print(img)
+        print(layer)
+        pdb.plug_in_autocrop_layer(img, layer)
+        print('Offset: {x}, {y}'.format(x=x, y=y))
+        pdb.gimp_layer_set_offsets(layer, x, y)
+        
+    def export_sprite(self, layer):
+        ''' Crop and export layer to png '''
+        name = '{name}.png'.format(name=layer.name)
+        print(name)
+        pdb.gimp_image_set_active_layer(self.img, layer)
+        # Find the layer position
+        offset = layer.offsets
+        width = layer.width / 2 + offset[0]
+        height = layer.height / 2 + offset[1]
+        pos = pdb.gimp_image_get_item_position(self.img, layer)
+        self.sprites[name] = Sprite(name)
+        self.sprites[name].pos = [width, pos, height]
+        self.sprites[name].info()
+        pdb.plug_in_autocrop_layer(self.img, layer)
+        # Copy layer into new image and save it out
+        pdb.gimp_edit_copy(layer)
+        imgtmp = pdb.gimp_edit_paste_as_new()
+        self.save_png(imgtmp, '/tmp/{name}'.format(name=name))
+        pdb.gimp_image_delete(imgtmp)
+
+    def export_tiles(self, layer):
+        pass
+
+    def save_png(self, img, path):
+        ''' Save img to path as png '''
+        drw = pdb.gimp_image_active_drawable(img)
+        pdb.file_png_save2(img,
+                           drw,
+                           path,
+                           os.path.basename(path),
+                           False,
+                           9,
+                           False,
+                           False,
+                           False,
+                           False,
+                           False,
+                           1,
+                           True)
+        
+
+        
+    def export(self):
+        # TODO! Make this whole operation one undo
+        # Loop through visible layers
+        sprites = {}
+        for layer in self.img.layers:
+            if layer.visible:
+                if len(layer.children) < 1:
+                    self.export_sprite(layer)
+                else:
+                    # TODO! Possibly break this out too.
+                    # Calculate optimal tiles and image size.
+                    frames = len(layer.children)
+                    gridx = floor(sqrt(frames))
+                    gridy = ceil(frames / gridx)
+                    pdb.plug_in_autocrop_layer(self.img, layer)
+                    print('Layer Group: {x}, {y}'.format(x=layer.width, y=layer.height))
+                    img2 = gimp.Image(int(layer.width * gridx), int(layer.height * gridy))
+                    pdb.gimp_display_new(img2)
+                    col = 1
+                    row = 1
+                    print('GRID: {x}, {y}'.format(x=gridx, y=gridy))
+                    for child in layer.children:
+                        print('COL {col}, ROW {row}'.format(col=col, row=row))
+                        if len(child.children) > 0:
+                            print("Nested layer groups not supported, skipping layer")
+                            continue
+                        pdb.gimp_image_set_active_layer(self.img, child)
+                        pdb.plug_in_autocrop_layer(self.img, child)
+                        # TODO! The offset is a bit off.
+                        x_delta = child.offsets[0] - layer.offsets[0]
+                        y_delta = child.offsets[1] - layer.offsets[1]
+                        print('Offsets Delta: {x}, {y}'.format(x=x_delta, y=y_delta))
                         pdb.gimp_edit_copy(child)
-                        tmplayer = img2.new_layer('{name}_{col}_{row})'.format(name=child.name, col=col, row=row),
-                                                  width,
-                                                  height)
-                        floating_layer = pdb.gimp_edit_paste(tmplayer, True)
-                        pdb.gimp_floating_sel_anchor(floating_layer)
-                        # TODO! Position the pasted image correctly in the grid.
-                        # Start in new row next loop
-                        col = 1
-                        row = row + 1
-                pdb.gimp_display_new(img2)
-            else:
-                name = '{name}.png'.format(name=layer.name)
-                print(name)
-                pdb.gimp_image_set_active_layer(img, layer)
-                # Find the layer position
-                offset = layer.offsets
-                width = layer.width / 2 + offset[0]
-                height = layer.height / 2 + offset[1]
-                pos = pdb.gimp_image_get_item_position(img, layer)
-                sprites[name] = Sprite(name)
-                sprites[name].pos = [width, pos, height]
-                sprites[name].info()
-                pdb.plug_in_autocrop_layer(img, layer)
-                # Copy layer into new image and save it out
-                pdb.gimp_edit_copy(layer)
-                img2 = pdb.gimp_edit_paste_as_new()
-                drw2 = pdb.gimp_image_active_drawable(img2)
-                pdb.file_png_save2(img2,
-                                   drw2,
-                                   '/tmp/{layer}'.format(layer=name),
-                                   name,
-                                   False,
-                                   9,
-                                   False,
-                                   False,
-                                   False,
-                                   False,
-                                   False,
-                                   1,
-                                   True)
-                pdb.gimp_image_delete(img2)
+                        self.paste_layer(img2,
+                                         '{name}_{col}_{row}'.format(name=child.name, col=col, row=row),
+                                         (layer.width * (col - 1)) + x_delta,
+                                         (layer.height * (row - 1)) + y_delta)
+                        if col % gridx > 0:
+                            col = col + 1
+                        else:
+                            col = 1
+                            row = row + 1
 
 
 def show_error_msg( msg ):
@@ -151,6 +169,10 @@ def show_error_msg( msg ):
     pdb.gimp_message(msg)
     pdb.gimp_message_set_handler(origMsgHandler)
 
+def export_to_coatools(img, drw):
+    export = CoaExport(img)
+    export.export()
+    
 
 register(
     "python_fu_coatools",
