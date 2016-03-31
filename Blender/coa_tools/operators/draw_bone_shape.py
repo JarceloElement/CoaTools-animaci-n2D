d357d855312cfab5b47a165b21f11e7a2b02429d
ndee85 <andreasesau@gmail.com>
2016-03-31 04:49:47 +0200

    Forgot to push 2 new py files

---
+++ Blender/coa_tools/operators/draw_bone_shape.py
@@ -0,0 +1,45 @@
+'''
+Copyright (C) 2015 Andreas Esau
+andreasesau@gmail.com
+
+Created by Andreas Esau
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+    
+import bpy
+import bpy_extras
+import bpy_extras.view3d_utils
+from math import radians
+import mathutils
+from mathutils import Vector, Matrix, Quaternion
+import math
+import bmesh
+from bpy.props import FloatProperty, IntProperty, BoolProperty, StringProperty, CollectionProperty, FloatVectorProperty, EnumProperty, IntVectorProperty
+import os
+from bpy_extras.io_utils import ExportHelper, ImportHelper
+import json
+from bpy.app.handlers import persistent
+from .. functions import *
+
+
+
+class DrawBoneShape(bpy.types.Operator):
+    bl_idname = "bone.coa_draw_bone_shape" 
+    bl_label = "Draw Bone Shape"
+
+    def execute(self, context):
+        bpy.ops.object.coa_edit_mesh(mode="DRAW_BONE_SHAPE")
+        return {'FINISHED'}
+
